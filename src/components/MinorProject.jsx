7141266faf56ed37827ee93c0dba289da2e39efa
lautaromendieta18 <161169170+lautaromendieta18@users.noreply.github.com>
2024-12-10 13:55:08 -0300

    Fix projects grid

--- src/components/MinorProject.jsx
+++ src/components/MinorProject.jsx
@@ -1,7 +1,7 @@
 export default function MinorProject({ title, children, icons, to }) {
   return (
     <a target="_blank" rel="noopener noreferrer" href={to}>
-      <article className="w-full grid-cols-1 border-2 h-36 border-slate-700 rounded-lg p-8 hover:scale-105 transition-all cursor-pointer">
+      <article className="w-full grid-cols-1 border-2 h-fit md:h-36 border-slate-700 rounded-lg p-8 hover:scale-105 transition-all cursor-pointer">
         <h3 className="font-semibold text-md ">{title}</h3>
         <p>{children}</p>
         <div>{icons}</div>
